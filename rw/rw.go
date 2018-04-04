5b4a055e2e5aef60c5f6aaebdd6bad4436b49a29
nico <brun.nicolas21@gmail.com>
2018-04-04 16:19:38 +0200

    gofmt

--- rw/rw.go
+++ rw/rw.go
@@ -1,36 +1,36 @@
 package rw
 
 import (
-  "crypto/rand"
-  "io"
+	"crypto/rand"
+	"io"
 )
 
-type empty struct {}
+type empty struct{}
 
 func Rand() io.Reader {
-  return &empty{}
+	return &empty{}
 }
 
 func (e *empty) Read(bs []byte) (int, error) {
-  n, err := io.ReadFull(rand.Reader, bs)
-  return int(n), err
+	n, err := io.ReadFull(rand.Reader, bs)
+	return int(n), err
 }
 
 type bytes struct {
-  buffer []byte
+	buffer []byte
 }
 
 func Alea(n int) io.Reader {
-  bs := make([]byte, n)
+	bs := make([]byte, n)
 	io.ReadFull(rand.Reader, bs)
 
-  return &bytes{bs}
+	return &bytes{bs}
 }
 
 func Zero(n int) io.Reader {
-  return &bytes{make([]byte, n)}
+	return &bytes{make([]byte, n)}
 }
 
 func (b *bytes) Read(bs []byte) (int, error) {
-  return copy(bs, b.buffer), nil
+	return copy(bs, b.buffer), nil
 }
